2f76eac534946a7167bda7e55d784eea8bab8320
luarkian <kaype_x@hotmail.com>
2019-02-21 16:47:39 -0400

    socket + serial + view output in html
    problemas => duplica ou triplica a leitura na serial.

--- server.js
+++ server.js
@@ -1,22 +1,50 @@
-var http = require('http');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
 const SerialPort = require('serialport')
 const Readline = require('@serialport/parser-readline')
-const port = new SerialPort('\\\\.\\COM5', { baudRate: 9600 })
-var app = require('./config/express');
+const port = new SerialPort('\\\\.\\COM4', { baudRate: 9600 })
+
+
+//app.use(express.static('./public'));
+app.get('/',function (req , res){
+	res.sendFile(__dirname+'/public/index.html');
+})
+
+const io = require('socket.io')(http);
 const parser = new Readline()
 port.pipe(parser);
-var objeto;
+var objeto = {'sala':'','tag':''};
 
-// Serial
+
+
+port.write('ROBOT POWER ON\n'); 
+
+io.on('connection', (socket)=>{
+	console.log('new connection', socket.id);
+
+	/*	############## SOCKET DO CHAT ############
+	socket.on('message', (msg)=>{
+		console.log(msg)
+		io.emit('message',msg);
+	});
+	########################################*/
+	
+	/*/ Serial/*/
 parser.on('data', (recebido_arduino) => {
 	var leitura = recebido_arduino.split(':');
 	var tag = leitura[1].split('\r');
 	 objeto= {'sala':leitura[0],
 	 			'tag': tag[0]}; 			
 	console.log(objeto);
-	 		})
-port.write('ROBOT POWER ON\n');
+	io.emit('dados arduino' ,objeto);
+	objeto = {'sala':'','tag':''};
+	});
+	
+});
 
-http.createServer(app).listen(3000,function(){
+
+
+http.listen(3000,function(){
 	console.log('Servidor Iniciado.');
 });
